a61f846b49d763cb4c23cb6d28a62f0fcb941f67
girlscodeclublancaster <34590972+girlscodeclublancaster@users.noreply.github.com>
2018-03-14 13:20:39 -0400

    Add JS file

---
+++ GCC.js
@@ -0,0 +1,10 @@
+﻿function SendEmail(name, subj, text) {
+    emailjs.init("user_hBsFRb46nBL53r9gBtua0");
+    emailjs.send("gmail", "template_hdrKzGi0", { your_name: name, subject: subj, notes: text })
+  	.then(function () {
+  	    alert("Sent!");
+  	}, function (err) {
+  	    alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+  	});
+    return false;
+}
