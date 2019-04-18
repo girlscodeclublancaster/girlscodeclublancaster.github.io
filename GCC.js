f35e9d5add14dafc0f9517bd3aafaecb3db56dd5
girlscodeclublancaster <34590972+girlscodeclublancaster@users.noreply.github.com>
2019-04-18 19:29:36 -0400

    Update GCC.js

--- GCC.js
+++ GCC.js
@@ -11,7 +11,7 @@
 
 ﻿function GCCSendEmailToSomeone(email, name, subj, text) {
     emailjs.init("user_hBsFRb46nBL53r9gBtua0");
-    emailjs.send("gmail", "template_send_to_someone", { recipient: email, your_name: name, subject: subj, notes: text })
+    emailjs.send("gmail_gcc", "template_send_to_someone", { recipient: email, your_name: name, subject: subj, notes: text })
   	.then(function () {
   	    alert("Sent to inbox!");
   	}, function (err) {
