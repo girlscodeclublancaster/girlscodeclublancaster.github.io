60d54233c13e143ff8834999a9d912c96a40dce5
girlscodeclublancaster <34590972+girlscodeclublancaster@users.noreply.github.com>
2019-04-18 19:28:25 -0400

    Update GCC.js

--- GCC.js
+++ GCC.js
@@ -9,6 +9,17 @@
     return false;
 }
 
+﻿function GCCSendEmailToSomeone(email, name, subj, text) {
+    emailjs.init("user_hBsFRb46nBL53r9gBtua0");
+    emailjs.send("gmail", "template_send_to_someone", { recipient: email, your_name: name, subject: subj, notes: text })
+  	.then(function () {
+  	    alert("Sent to inbox!");
+  	}, function (err) {
+  	    alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+  	});
+    return false;
+}
+
 ﻿function SendEmailToSomeone(email, name, subj, text) {
     emailjs.init("user_hBsFRb46nBL53r9gBtua0");
     emailjs.send("gmail", "template_send_to_someone", { recipient: email, your_name: name, subject: subj, notes: text })
