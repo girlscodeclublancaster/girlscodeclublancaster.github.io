03430cab7d86953a511fece70e7b30abef21fcce
Girls Code Club Lancaster <34590972+girlscodeclublancaster@users.noreply.github.com>
2021-04-01 15:52:34 -0400

    Create Session7.js

---
+++ Session7.js
@@ -0,0 +1,10 @@
+﻿function SendEmail(email, name, subj, text) {
+    emailjs.init("user_hBsFRb46nBL53r9gBtua0");
+    emailjs.send("gmail", "template_send_to_someone", { recipient: email, your_name: name, subject: subj, notes: text })
+  	.then(function () {
+  	    alert("Sent to inbox!");
+  	}, function (err) {
+  	    alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+  	});
+    return false;
+}
