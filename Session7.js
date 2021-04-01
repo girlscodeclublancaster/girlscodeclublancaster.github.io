ab9ba03ffc888fb7500d97c5fd2a573a79769bce
Girls Code Club Lancaster <34590972+girlscodeclublancaster@users.noreply.github.com>
2021-04-01 16:14:58 -0400

    Update Session7.js

--- Session7.js
+++ Session7.js
@@ -1,6 +1,6 @@
 ﻿function SendEmail(email, name, subj, text) {
     emailjs.init("user_hBsFRb46nBL53r9gBtua0");
-    emailjs.send("gmail", "template_send_to_someone", { recipient: email, your_name: name, subject: subj, notes: text })
+    emailjs.send("service_1v4xyvq", "template_send_to_someone", { recipient: email, your_name: name, subject: subj, notes: text })
   	.then(function () {
   	    alert("Sent to inbox!");
   	}, function (err) {
