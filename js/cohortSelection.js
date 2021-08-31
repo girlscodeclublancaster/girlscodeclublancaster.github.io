2b5664ac25b29c9ade53ecdc955ce4823795f646
Emmali Montgomery <emmalimontgomery@Emmalis-MacBook-Pro.local>
2021-08-31 11:00:04 -0400

    gcc website v6

---
+++ js/cohortSelection.js
@@ -0,0 +1,60 @@
+// function that will allow us to import JS from other files
+function include(file) {
+    var script  = document.createElement('script');
+    script.src  = file;
+    script.type = 'text/javascript';
+    script.defer = true;
+    document.getElementsByTagName('head').item(0).appendChild(script);
+}
+
+// 1. Start at nonexistent number - GREATER than # of cohorts.
+var selectedCohort = 5;
+
+// 2. Include times for each cohort (in order).
+include('./js/SessionTimes_20212022.js');
+
+// 3. Include all sessions for all cohorts (in order). Each cohort must have the same number of sessions.
+include('./js/SessionDates_20212022.js');
+
+var selectedDates = []; // dates to be shown on website
+
+// 4. User clicks a cohort (date/time) - this function decides what dates show and displays them on the page.
+function selectCohort(cohortNum) {
+    if (selectedCohort === cohortNum) {
+        // user has clicked a date/time that is already open - close it
+        document.getElementById('cohortDates').innerHTML = '';
+        document.getElementById('cohortTime').innerHTML = '';
+        document.getElementById('cohortContent').classList.remove('cohortContentDisplay');
+        document.getElementById(`cohort${cohortNum}`).classList.remove('selectedButton');
+        selectedCohort = 5;
+        selectedDates = [];
+    } else {
+        // new date/time selection - open it on the page
+        // determine the selected dates
+        selectedDates = sessions.filter((session, index) => (index % 3) === cohortNum );
+
+        // set up HTML for the page
+        var innerHTML = '';
+        selectedDates.forEach(date => {
+            innerHTML += `<div><i class="fas fa-code"></i>${date}${date.includes("Nov") || date.includes("Dec") ? "*" : ""}</div>`;
+        });
+        
+        // inject new HTML into the page based on selection
+        document.getElementById('cohortDates').innerHTML = innerHTML;
+        document.getElementById('cohortTime').innerHTML = `<div>${times[cohortNum]}</div>`;
+        document.getElementById('cohortContent').classList.add('cohortContentDisplay');
+
+        // set UI for selected cohort
+        document.getElementById(`cohort${cohortNum}`).classList.add('selectedButton');
+
+        // unset UI for previously selected cohort
+        var previouslySelected = document.getElementById(`cohort${selectedCohort}`);
+        if (previouslySelected) {
+            previouslySelected.classList.remove('selectedButton');
+        } else {
+            // all cohorts were previously unselected
+        }
+        
+        selectedCohort = cohortNum;
+    }
+}
