135adbcef6a9bc86c8b85011c5a7ab1502ef6e12
Emmali Montgomery <emmalit@Emmas-Air.local>
2022-10-22 19:00:19 -0400

    session 2 added

---
+++ assets/js/cohorts.js
@@ -0,0 +1,125 @@
+// months are 0 - 11
+var d = new Date();
+
+var cohort1Time = "Sundays @ 1:00PM - 2:30PM";
+var cohort1Title = "Intermediate to Advanced Coderss";
+var cohort1Dates = [
+    {
+        date: d.setFullYear(2022, 8, 18),
+        dateString: "September 18th, 2022"
+    },
+    {
+        date: d.setFullYear(2022, 9, 23),
+        dateString: "October 23rd, 2022"
+    },
+    {
+        date: d.setFullYear(2022, 10, 20),
+        dateString: "November 20th, 2022"
+    },
+    {
+        date: d.setFullYear(2022, 11, 11),
+        dateString: "December 11th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 0, 15),
+        dateString: "January 15th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 1, 12),
+        dateString: "February 12th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 2, 12),
+        dateString: "March 12th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 3, 16),
+        dateString: "April 16th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 4, 14),
+        dateString: "May 14th, 2022"
+    }
+];
+
+var cohort2Time = "Sundays @ 3:00PM - 4:30PM";
+var cohort2Title = "Beginner Coders";
+var cohort2Dates = [
+    {
+        date: d.setFullYear(2022, 8, 18),
+        dateString: "September 18th, 2022"
+    },
+    {
+        date: d.setFullYear(2022, 9, 23),
+        dateString: "October 23rd, 2022"
+    },
+    {
+        date: d.setFullYear(2022, 10, 20),
+        dateString: "November 20th, 2022"
+    },
+    {
+        date: d.setFullYear(2022, 11, 11),
+        dateString: "December 11th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 0, 15),
+        dateString: "January 15th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 1, 12),
+        dateString: "February 12th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 2, 12),
+        dateString: "March 12th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 3, 16),
+        dateString: "April 16th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 4, 14),
+        dateString: "May 14th, 2022"
+    }
+];
+
+var cohort3Time = "Wednesdays @ 4:00PM - 5:30PM";
+var cohort3Title = "All Experience Levels";
+var cohort3Dates = [
+    {
+        date: d.setFullYear(2022, 8, 21),
+        dateString: "September 21st, 2022"
+    },
+    {
+        date: d.setFullYear(2022, 9, 19),
+        dateString: "October 19th, 2022"
+    },
+    {
+        date: d.setFullYear(2022, 10, 16),
+        dateString: "November 16th, 2022"
+    },
+    {
+        date: d.setFullYear(2022, 11, 14),
+        dateString: "December 14th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 0, 18),
+        dateString: "January 18th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 1, 15),
+        dateString: "February 15th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 2, 15),
+        dateString: "March 15th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 3, 19),
+        dateString: "April 19th, 2022"
+    },
+    {
+        date: d.setFullYear(2023, 4, 17),
+        dateString: "May 17th, 2022"
+    }
+];
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,7 +1,18 @@
-var currentNav = "home";
+// year Girls Code Club starts
+var beginYear = 2022;
 // "home", "schedule", or "lessons"
+var currentNav = "home";
+var href = window.location.href;
+var setCollapsible = false;
+
+window.addEventListener('load', function () {
+    // based on url, set initial tab
+    setInitialTab();
+})
 
 function selectNav(navItem) {
+    parent.location.hash = navItem;
+
     // deselect old UI
     document.getElementById(currentNav).classList.add("hidden");
     var nav = currentNav + "-nav";
@@ -13,4 +24,89 @@ function selectNav(navItem) {
     // reselect new UI
     document.getElementById(currentNav).classList.remove("hidden");
     document.getElementById(nav).classList.add("navUnderline");
+    if (navItem === "lessons" && !setCollapsible) {
+        collapsibleLessons();
+    }
+}
+
+function setInitialTab() {
+    if (href.includes("lessons")) {
+        selectNav("lessons");
+    } else if (href.includes("schedule")) {
+        selectNav("schedule");
+    } else {
+        selectNav("home")
+    }
+}
+
+// set up collapsible UI for lessons
+function collapsibleLessons() {
+    setCollapsible = true;
+    var coll = document.getElementsByClassName("collapsible");
+
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+
+    autoOpenLesson(coll);
+}
+
+// auto-open the lesson tab for this month
+// months go from 0 - 11
+function autoOpenLesson(coll) {
+    var today = new Date;
+    var month = today.getMonth();
+    var year = month >= 8 && month <= 11 ? beginYear : beginYear + 1;
+    var dateCompare = month + "-" + year;
+    console.log(dateCompare)
+    
+    switch(dateCompare) {
+        case "8-2022":
+            // September - Session 1
+            coll[0].click();
+            break;
+        case "9-2022":
+            // October - Session 2
+            coll[1].click();
+            break
+        case "10-2022":
+            // November - Session 3
+            coll[2].click();
+            break;
+        case "11-2022":
+            // December - Session 4
+            coll[3].click();
+            break;
+        case "0-2023":
+            // January - Session 5
+            coll[4].click();
+            break;
+        case "1-2023":
+            // February - Session 6
+            coll[5].click();
+            break;
+        case "2-2023":
+            // March - Session 7
+            coll[6].click();
+            break;
+        case "3-2023":
+            // April - Session 8
+            coll[7].click();
+            break;
+        case "4-2023":
+            // May - Session 9
+            coll[8].click();
+            break;
+        default:
+            break;
+    }
+
 }
