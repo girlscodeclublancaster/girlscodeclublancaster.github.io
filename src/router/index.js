9670949b16e0a1c8895bcec556d8c496ba819310
Emmali Montgomery <emmalit@Emmas-Air.local>
2024-01-13 13:36:00 -0500

    added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import AboutView from '../views/AboutView.vue'
 import ScheduleView from '../views/ScheduleView.vue'
 import LessonsView from '../views/LessonsView.vue'
 import SHEroView from '../views/SHEroView.vue'
+import PageNotFound from '../views/404NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,11 +14,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: AboutView
+    // },
     {
       path: '/schedule',
       name: 'schedule',
@@ -32,7 +33,8 @@ const router = createRouter({
       path: '/shero',
       name: 'shero',
       component: SHEroView
-    }
+    },
+    { path: "/:pathMatch(.*)*", component: PageNotFound }
   ]
 })
 
---
+++ src/views/404NotFoundView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="notfound">
+    <h1>404: Page Not Found</h1>
+    <div class="line"></div>
+    <div class="pageContent">
+      <p>Oops! We don't know what to do with this URL. Use our navigation up top to learn more about Girls Code Club.</p>
+      <div class="notFoundAnimation">
+        <lottie-player src="https://assets9.lottiefiles.com/private_files/lf30_tonsVH.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    margin: 0px !important;
+    color: black;
+    padding-top: 2rem;
+}
+
+.pageContent {
+    display: flex;
+    flex-direction: column !important;
+    align-items: center !important;
+}
+
+p {
+    text-align: center;
+    margin: 1rem;
+}
+
+.notfound {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start !important;
+    align-items: center !important;
+}
+
+@media (min-width: 1024px) {
+  .notfound {
+    min-height: 100vh;
+  }
+}
+</style>
