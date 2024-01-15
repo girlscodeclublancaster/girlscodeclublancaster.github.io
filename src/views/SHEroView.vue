148d689ed7bbc9d42c06f04a8c4726c0559fda0b
Emmali Montgomery <emmalit@Emmas-Air.local>
2024-01-14 19:34:42 -0500

    all shero graphics added

--- src/views/SHEroView.vue
+++ src/views/SHEroView.vue
@@ -6,7 +6,7 @@ import TheWelcome from '../components/TheWelcome.vue'
   <main>
     <div class="sheroes">
       <h2 class="boldedFont">SHEroes (Female Role-Models in Tech)</h2>
-      <p>This page will feature all SHEroes that have been presented to Girls Code Club. All graphics have been drawn by Head Coach, Emma Wertz, which is why some images are missing. This page is in progress! Information about each SHEro will also be included.</p>
+      <p>Please meet the SHEroes that have been presented to Girls Code Club over the years. All graphics have been drawn by Head Coach, Emma Wertz! This page is still in progress; come back soon to view details on each SHEro.</p>
       <div class="shero-container">
         <div v-for="shero in sheroesSorted" :key="shero.id">
           <Shero
@@ -37,7 +37,7 @@ export default {
           name: "Ada Lovelace",
           born: "12/10/1815",
           died: "11/27/1852",
-          img: "comingsoon.png",
+          img: "ada.png",
           description: "From a very young age, Ada was invested in the world of math and science. Her mother felt this could help Ada avoid walling into the madness her poet father was perceived to endure. Once she'd reached adulthood, Ada used her math skills to be more involved in computers. She had written a series of notes where one of them is considered to contain the first EVER computer program.",
           link: "https://en.wikipedia.org/wiki/Ada_Lovelace",
           video: "https://www.youtube.com/watch?v=uOkmyICUW_c"
@@ -83,56 +83,56 @@ export default {
         {
           id: 7,
           name: "Gladys West",
-          img: "comingsoon.png",
+          img: "gladys.png",
           description: "",
           link: ""
         },
         {
           id: 8,
-          name: "Saffi Goldwasser",
-          img: "comingsoon.png",
+          name: "Shaffi Goldwasser",
+          img: "shaffi.png",
           description: "",
           link: ""
         },
         {
           id: 9,
-          name: "Joan Clark",
-          img: "comingsoon.png",
+          name: "Joan Clarke",
+          img: "joanclarke.png",
           description: "",
           link: ""
         },
         {
           id: 10,
           name: "Radia Joy Perlman",
-          img: "comingsoon.png",
+          img: "radia.png",
           description: "",
           link: ""
         },
         {
           id: 11,
           name: "Katherine Johnson",
-          img: "comingsoon.png",
+          img: "katherine.png",
           description: "",
           link: ""
         },
         {
           id: 12,
           name: "Adele Goldberg",
-          img: "comingsoon.png",
+          img: "adele.png",
           description: "",
           link: ""
         },
         {
           id: 13,
           name: "Karen Spark Jones",
-          img: "comingsoon.png",
+          img: "karen.png",
           description: "",
           link: ""
         },
         {
           id: 14,
           name: "Stephanie Shirley",
-          img: "comingsoon.png",
+          img: "stephanie.png",
           description: "",
           link: ""
         },
